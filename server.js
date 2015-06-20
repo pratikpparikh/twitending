e052e0b24803786d654ba2fd9d95f631697a19b5
Pratik Parikh <parikh@hp.com>
2015-06-20 15:04:04 -0400

    Signed-off-by: Pratik Parikh <parikh@hp.com>

--- server.js
+++ server.js
@@ -16,41 +16,43 @@ var parseTweets = function(body) {
   var tweets = [];
 
   $('ol.stream-items li.stream-item').each(function(i, element) {
-    var id = $(this).attr('data-item-id');
-    var time = new Date();
-    var thumbnailurl = "";
-    var videourl = "";
-    if($(this).find("div[data-card-type='photo']")){
-    	thumbnailurl =  $('.media-thumbnail', this).attr('data-url');
-    }else if($(this).find("div[data-card-type='animated_gif']")){
-    	thumbnailurl =  $('video', this).attr('poster');
-    	videourl =  $('video', this).attr('src');
-    }else if($(this).find("div[data-card-name='player']")){
-    	videourl =  $('.js-macaw-cards-iframe-container iframe', this).attr('src');
-    }
-    if($(this).find("div.content div.stream-item-header small.time a span")){
-    	time = $('.js-short-timestamp', this).attr('data-time');
-    }
-    var text = $('.js-tweet-text', this).text();
-    text = text.replace('â€¦', '');
-    text = text.trim();
-    if(typeof text != 'undefined' && text != null && text != ''){
-	    tweets.push({
-	      createdAt: parseInt(time, 10),
-	      id: id,
-	      text: text,
-	      thumbnail: thumbnailurl,
-	      video:videourl
-	    });
-  	}
-  });
+	    var id = $(this).attr('data-item-id');
+	    var time = new Date();
+	    var thumbnailurl = "";
+	    var videourl = "";
+	    
+	    
+	    if($(this).find("div[data-card-type='photo']").length > 0){
+	    	thumbnailurl =  $('.media-thumbnail', this).attr('data-url');
+	    }else if($(this).find("div[data-card2-type='animated_gif']").length > 0){
+	    	thumbnailurl =  $('video', this).attr('poster');	    	
+	    	videourl =  $('source', this).attr('video-src');
+	    }else if($(this).find("div[data-card-name='player']").length > 0){
+	    	videourl =  "https://twitter.com" + $('.js-macaw-cards-iframe-container', this).attr('data-full-card-iframe-url');
+	    }
+	    if($(this).find("div.content div.stream-item-header small.time a span")){
+	    	time = $('.js-short-timestamp', this).attr('data-time');
+	    }
+	    var text = $('.js-tweet-text', this).text();
+	    text = text.replace('â€¦', '');
+	    text = text.trim();
+	    if(typeof text != 'undefined' && text != null && text != ''){
+		    tweets.push({
+		      createdAt: parseInt(time, 10),
+		      id: id,
+		      text: text,
+		      thumbnail: thumbnailurl,
+		      video:videourl
+		    });
+	  	}
+	  });
 
   return tweets;
 };
 
 var getTweets = function(res, id) {
   request({
-    url: 'https://twitter.com/whatstrending' + !id || id.length > 0 ? "/" + id : "" 
+    url: 'https://twitter.com/whatstrending' + (id === 'undefined' || id.length == 0 ? "" : "/" + id) 
   }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var tweets = parseTweets(body);
@@ -80,11 +82,11 @@ app.use(allowCrossDomain);
 app.get('/:id?', function(req, res){
   var id = req.params.id;
   var tweets = [];
+
   if (!id) {
-	id = "";
-	tweets = getTweets(res, id);
-  }else{
-    tweets = getTweets(res, id);
+	  tweets = getTweets(res, "");
+  } else {
+	  tweets = getTweets(res, id);
   }
 
 });
